c3f02311a8eadc88a06d595a82ee88e698cee7e0
robstarbuck <robstarbuck@gmail.com>
2022-04-27 17:07:05 +0100

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { FC } from "react";
+import "./App.css";
+import allTrees from "./tree-data.json";
+
+type Tree = Omit<typeof allTrees[number], "(unranked)" | "Division" | "Class">;
+type TaxonomicRank = keyof Tree;
+
+// domain, kingdom, phylum, class, order, family, genus, and species
+
+const taxonomies: Array<TaxonomicRank> = [
+  "Kingdom",
+  "Order",
+  "Family",
+  "Genus",
+  "Name",
+];
+
+const Taxonomy: FC<{ trees: Array<Tree>; rank: TaxonomicRank }> = (props) => {
+  const { trees, rank } = props;
+
+  const taxons = trees.reduce<Array<Tree>>((a, c) => {
+    if (a.find((v) => v[rank].value === c[rank].value)) {
+      return a;
+    }
+    return [...a, c];
+  }, []);
+
+  return (
+    <div title={rank}>
+      {taxons.map((taxon) => {
+        const treesInTaxon = trees.filter(
+          (t) => t[rank].value === taxon[rank].value
+        );
+        const subRank = taxonomies[taxonomies.indexOf(rank) + 1];
+
+        if (!subRank) {
+          return (
+            <div style={{ paddingLeft: 20 }}>
+              <a href={taxon[rank].href}>{taxon[rank].value}</a>
+            </div>
+          );
+        }
+        return (
+          <details open>
+            <summary>
+              <a href={taxon[rank].href} target="_blank">{taxon[rank].value}</a>
+            </summary>
+            <Taxonomy trees={treesInTaxon} rank={subRank} />
+          </details>
+        );
+      })}
+    </div>
+  );
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Taxonomy rank={taxonomies[0]} trees={allTrees} />
     </div>
   );
 }
