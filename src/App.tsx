deeea68c90a00caeedb41f764ebd286d33d03785
robstarbuck <robstarbuck@gmail.com>
2022-04-29 00:25:56 +0100

    Update to style

--- src/App.tsx
+++ src/App.tsx
@@ -6,13 +6,15 @@ type Tree = Omit<typeof allTrees[number], "(unranked)" | "Division" | "Class">;
 type TaxonomicRank = keyof Tree;
 
 // domain, kingdom, phylum, class, order, family, genus, species
-const taxonomies: Array<TaxonomicRank> = [
+const taxonomiesX: Array<TaxonomicRank> = [
   "Kingdom",
   "Order",
   "Family",
   "Genus",
   "Name",
 ];
+
+const taxonomies = taxonomiesX;
 
 const Taxonomy: FC<{ trees: Array<Tree>; rank: TaxonomicRank }> = (props) => {
   const { trees, rank } = props;
@@ -31,24 +33,16 @@ const Taxonomy: FC<{ trees: Array<Tree>; rank: TaxonomicRank }> = (props) => {
           (t) => t[rank].value === taxon[rank].value
         );
         const subRank = taxonomies[taxonomies.indexOf(rank) + 1];
+        const hasSubrank = subRank !== undefined;
 
-        if (!subRank) {
-          return (
-            <div>
-              <em>
-                <a href={taxon[rank].href} title={taxon.Species.value}>{taxon[rank].value}</a>
-              </em>
-            </div>
-          );
-        }
         return (
-          <details open>
+          <details open={hasSubrank}>
             <summary>
               <a href={taxon[rank].href} target="_blank">
                 {taxon[rank].value}
               </a>
             </summary>
-            <Taxonomy trees={treesInTaxon} rank={subRank} />
+            {hasSubrank && <Taxonomy trees={treesInTaxon} rank={subRank} />}
           </details>
         );
       })}
@@ -57,33 +51,35 @@ const Taxonomy: FC<{ trees: Array<Tree>; rank: TaxonomicRank }> = (props) => {
 };
 
 function App() {
-
   const [startRank, setStartRank] = useState<TaxonomicRank>(taxonomies[0]);
 
   const onSetStartRank = (rank: TaxonomicRank) => {
-    setStartRank(rank)
-  }
+    setStartRank(rank);
+  };
 
   const parentTaxonomy = taxonomies[taxonomies.indexOf(startRank) - 1];
-  const curtailedTaxonomies = taxonomies.slice(taxonomies.indexOf(startRank))
+  const curtailedTaxonomies = taxonomies.slice(taxonomies.indexOf(startRank));
 
   return (
     <div className="App">
       <header>
-        {parentTaxonomy && <button onClick={() => onSetStartRank(parentTaxonomy)}>
-          ...{parentTaxonomy}
-        </button>}
+        {parentTaxonomy && (
+          <button onClick={() => onSetStartRank(parentTaxonomy)}>
+            ...{parentTaxonomy}
+          </button>
+        )}
         <ol>
-          {curtailedTaxonomies.map(taxonomy => 
-          <li>
-            <button onClick={() => onSetStartRank(taxonomy)}>
-              {taxonomy}
-            </button>
-          </li>)}
+          {curtailedTaxonomies.map((taxonomy) => (
+            <li>
+              <button onClick={() => onSetStartRank(taxonomy)}>
+                {taxonomy}
+              </button>
+            </li>
+          ))}
         </ol>
       </header>
       <main>
-      <Taxonomy rank={curtailedTaxonomies[0]} trees={allTrees} />
+        <Taxonomy rank={curtailedTaxonomies[0]} trees={allTrees} />
       </main>
     </div>
   );
